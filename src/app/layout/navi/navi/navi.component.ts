2fb02e09e7f0ede60ffb83e6d756c60c39ec6a01
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2022-12-28 22:15:13 +0300

    update navi

--- src/app/layout/navi/navi/navi.component.ts
+++ src/app/layout/navi/navi/navi.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  logOut(){
+
   }
 
 }
